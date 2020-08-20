0f985685e4cb0d5812d9b3078302d3f5f913c953
Lowlet <maximko1993@gmail.com>
2020-08-20 14:45:39 +0300

    Update

--- Build/MahouShoujoWardrobeBuild.framework.js
+++ Build/MahouShoujoWardrobeBuild.framework.js
@@ -1299,7 +1299,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 368e4;
+STATICTOP = STATIC_BASE + 3680288;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3229,7 +3229,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 368e4;
+var STATIC_BUMP = 3680288;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
